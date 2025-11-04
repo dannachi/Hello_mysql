11784d11cd85d74df6f809fa7c154fe71444990b
Daniel Rodriguez <daniel.rodriguez.sanchez@correounivalle.edu.co>
2025-11-04 15:07:42 -0500

    DELETE FROM users WHERE id_user = 5
    writing y reading

---
+++ Writing/01_insert_into.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id_user, name, last_name) VALUES (7, "Camila", "Acevedo")
+
+INSERT INTO users (name, last_name) VALUES ("Juan", "Cuartas")
---
+++ Writing/02_update.sql
@@ -0,0 +1,3 @@
+UPDATE users SET age = 21 WHERE id_user = 1
+
+UPDATE users SET age = 20, init_date = '2024-06-12' WHERE id_user = 2
