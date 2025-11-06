d192dd1f329692c58080486fcdf5ce74df5c8a1e
Daniel Rodriguez <daniel.rodriguez.sanchez@correounivalle.edu.co>
2025-11-06 07:47:40 -0500

    inserts a tablas

--- Writing/01_insert_into.sql
+++ Writing/01_insert_into.sql
@@ -1,3 +1,5 @@
 INSERT INTO users (id_user, name, last_name) VALUES (7, "Camila", "Acevedo")
 
 INSERT INTO users (name, last_name) VALUES ("Juan", "Cuartas")
+
+INSERT INTO cedula (cedula_number, id_user) VALUES (1193434377, 2);
